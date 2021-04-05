2e70ca417ac748d9d7954b1805784805159e8051
thaivan <giacatluong20121996@gmail.com>
2021-04-05 19:44:21 +0700

    ...

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="about">
     <h3>{{getCurrentClicker}}</h3>
-    <Clicker index="1"/>
-    <Clicker index="2"/>
-    <Clicker index="3"/>
+
+    <div class="row-clicker">
+      <Clicker index="1"/>
+      <Clicker index="2"/>
+      <Clicker index="3"/>
+    </div>
+
+    <div class="column-clicker">
+      <!-- <Clicker index="1"/> -->
+      <Clicker index="2"/>
+      <Clicker index="3"/>
+    </div>
   </div>
 </template>
 
@@ -26,8 +35,24 @@ export default {
 
 <style lang="scss">
 .about {
-  .clicker:not(:nth-child(1)) {
+
+  >.column-clicker {
     margin-top: 30px;
+
+    >.clicker:not(:nth-child(1)) {
+      margin-top: 30px;
+    }
+  
   }
+
+  >.row-clicker {
+    display: flex;
+
+    >.clicker:not(:nth-child(1)) {
+      margin-left: 30px;
+    }
+  
+  }
+
 }
 </style>
